68b30c387bc05475dde02b99cfe89397bcbc0896
Ogundare Samuel Olamide <oluwadareysamuel@yahoo.com>
2020-01-27 15:59:06 +0100

    homepage about section created

---
+++ src/components/About.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const About = () => {
+ return (
+  <div className="about">
+   <h2 className="sub-header">ABOUT</h2>
+   <hr />
+   <div className="about__text">
+    <p className="para2">
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam error nemo fuga possimus modi? Cumque voluptatem consequuntur quia, similique aperiam inventore labore? Quos voluptas tempora odio vel laudantium praesentium, molestiae maiores optio cupiditate explicabo dolores amet distinctio fugiat dolorem perferendis saepe accusamus laborum ab quo eaque maxime dignissimos molestias! Pariatur tenetur.
+    </p>
+   </div>
+  </div>
+ )
+}
+
+export default About;
--- src/container/Savus.js
+++ src/container/Savus.js
@@ -1,5 +1,6 @@
 import React, { Component , Fragment } from 'react';
 import Header from '../components/Header';
+import About from '../components/About';
 import '../css/style.css';
 
 class Savus extends Component {
@@ -8,6 +9,7 @@ class Savus extends Component {
   return (
    <Fragment>
     <Header />
+    <About />
    </Fragment>
   )
  }

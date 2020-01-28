08c9122d9b21a9f872c0dfae2bb9acfa63a038b4
Ogundare Samuel Olamide <oluwadareysamuel@yahoo.com>
2020-01-28 05:45:34 +0100

    link to switch to SignupModal added to LoginModal

--- src/components/LoginModal.js
+++ src/components/LoginModal.js
@@ -2,27 +2,33 @@ import React, { Fragment } from 'react'
 import Backdrop from '../UI/Backdrop';
 
 const LoginModal = (props) => {
-return (
- <Fragment>
-  <Backdrop>
-  <div className="login-container">
-    <h3 className="sub-header">Login</h3>
-    <span className="close">X</span>
-    <form>
-     <div className="form-group">
+ return (
+  <Fragment>
+   <Backdrop>
+    <div className="login-container">
+     <h3 className="sub-header">Login</h3>
+     <span className="closelogin close">X</span>
+     <form>
+      <div className="form-group">
        <input type="email" name="login-email" placeholder="Email" required />
-     </div>
-     <div className="form-group">
+      </div>
+      <div className="form-group">
        <input type="password" name="login-password" placeholder="Password" required />
-     </div>
-     <div className="form-group">
+      </div>
+      <div className="form-group">
        <input type="submit" name="login-submit" value="Login" />
-     </div>
-    </form>
-  </div>
-  </Backdrop>
- </Fragment>
-)
+      </div>
+      <div className="toggleModal">
+       <a href="#" id="signup" className="signup">
+        Need and account? <strong>Register here!</strong>
+      </a>
+      </div>
+
+     </form>
+    </div>
+   </Backdrop>
+  </Fragment>
+ )
 }
 
 export default LoginModal;
